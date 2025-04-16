e5a9c7b3c08e026e95507096374a60fab3f26520
ManavPatni <developer.mnvpatni@gmail.com>
2025-04-16 23:11:48 +0530

    feat: Implement Pass and Coupon Fragments with Bottom Navigation

--- app/src/main/java/com/devmnv/prabal25/MainActivity.kt
+++ app/src/main/java/com/devmnv/prabal25/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.devmnv.prabal25
 
 import android.os.Bundle
-import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.devmnv.prabal25.databinding.ActivityMainBinding
+import com.devmnv.prabal25.fragment.CouponFragment
 import com.devmnv.prabal25.fragment.HomeFragment
+import com.devmnv.prabal25.fragment.PassFragment
 import com.devmnv.prabal25.sharedPrefs.AuthSharedPref
 
 class MainActivity : AppCompatActivity() {
@@ -16,24 +14,85 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var authSharedPref: AuthSharedPref
 
+    private var homeFragment: HomeFragment? = null
+    private var passFragment: PassFragment? = null
+    private var couponFragmentTag = "coupon_fragment"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Init sharedPrefs
         authSharedPref = AuthSharedPref(this)
 
-        // Default fragment
-        replaceFragment(HomeFragment())
+        // Init fragments
+        homeFragment = HomeFragment()
+        passFragment = PassFragment()
 
-
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
+        // Load default fragment
         supportFragmentManager.beginTransaction()
-            .replace(R.id.frameLayout, fragment)
+            .add(R.id.frameLayout, homeFragment!!, "home")
             .commit()
+
+        setupBottomNav()
     }
 
+    private fun setupBottomNav() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    showFragment(homeFragment!!)
+                    true
+                }
+                R.id.nav_pass -> {
+                    showFragment(passFragment!!)
+                    true
+                }
+                R.id.nav_coupon -> {
+                    // Always refresh CouponFragment
+                    val couponFragment = CouponFragment()
+                    replaceFragment(couponFragment, couponFragmentTag)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        // Hide all existing fragments
+        for (frag in supportFragmentManager.fragments) {
+            transaction.hide(frag)
+        }
+
+        // Add or show the selected fragment
+        if (!fragment.isAdded) {
+            transaction.add(R.id.frameLayout, fragment)
+        } else {
+            transaction.show(fragment)
+        }
+
+        transaction.commit()
+    }
+
+    private fun replaceFragment(fragment: Fragment, tag: String) {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        // Remove old coupon fragment if it exists
+        val oldCoupon = supportFragmentManager.findFragmentByTag(tag)
+        if (oldCoupon != null) {
+            transaction.remove(oldCoupon)
+        }
+
+        transaction.add(R.id.frameLayout, fragment, tag)
+
+        // Hide all other fragments
+        for (frag in supportFragmentManager.fragments) {
+            if (frag != fragment) transaction.hide(frag)
+        }
+
+        transaction.commit()
+    }
 }
---
+++ app/src/main/java/com/devmnv/prabal25/fragment/PassFragment.kt
@@ -0,0 +1,75 @@
+package com.devmnv.prabal25.fragment
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.devmnv.prabal25.R
+import com.devmnv.prabal25.auth.AuthManager
+import com.devmnv.prabal25.databinding.FragmentHomeBinding
+import com.devmnv.prabal25.databinding.FragmentPassBinding
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.common.BitMatrix
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class PassFragment : Fragment() {
+
+    private var _binding: FragmentPassBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var qrBitmap: Bitmap
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPassBinding.inflate(inflater, container, false)
+
+        // Generate QR code in background thread
+        CoroutineScope(Dispatchers.IO).launch {
+            qrBitmap = generateQRCode()
+            withContext(Dispatchers.Main) {
+                Glide.with(requireContext())
+                    .load(qrBitmap)
+                    .into(binding.ivQr)
+            }
+        }
+
+        return binding.root
+    }
+
+    // Function to generate the QR code bitmap
+    private fun generateQRCode(): Bitmap {
+        val width = 400
+        val height = 400
+        val bitMatrix: BitMatrix = MultiFormatWriter().encode(
+            AuthManager.getTeamId(requireContext()),
+            BarcodeFormat.QR_CODE,
+            width,
+            height
+        )
+
+        // Use ARGB_8888 to support transparency
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+
+        // Your desired QR color
+        val qrColor = Color.parseColor("#E047280B")
+
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                bitmap.setPixel(x, y, if (bitMatrix[x, y]) qrColor else Color.TRANSPARENT)
+            }
+        }
+
+        return bitmap
+    }
+
+}
